d18c1f3a36557c4bf15bc99b780248513f066bbc
rjd2000 <borahrajdeep726@gmail.com>
2025-07-18 20:13:36 +0530

    Update training.js

--- route/training.js
+++ route/training.js
@@ -3,10 +3,10 @@ const db = require('../database');
 const router = express.Router();
  
 router.get('/tra', (req, res) => {
-    const query = 'SELECT * FROM training ';
+    const query = 'SELECT * FROM Training ';
     db.query(query, (err, results) => {
         if (err) {
-            console.error('Error fetching training :', err);
+            console.error('Error fetching Training :', err);
             return res.status(500).json({ error: 'Internal server error' });
         }
         res.json(results);
