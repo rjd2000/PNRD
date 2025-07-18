7e659de82f5f9eb5013155280bd16740479fca3d
rjd2000 <borahrajdeep726@gmail.com>
2025-07-18 23:25:33 +0530

    Create employee.js

---
+++ employee.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('../database');
+const router = express.Router();
+
+router.get('em', (req, res) => {
+    const query = 'SELECT * FROM employee';
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error fetching employee:', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.json(results);
+    });
+})
+
+
+module.exports = router;
