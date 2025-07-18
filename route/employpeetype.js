2ee02b0eb0457006dc1ab15d45195d4368b93fac
rjd2000 <borahrajdeep726@gmail.com>
2025-07-18 16:36:16 +0530

    Create employpeetype.js

---
+++ route/employpeetype.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('../database');
+const router = express.Router();
+ 
+router.get('/ty', (req, res) => {
+    const query = 'SELECT * FROM employeetype ';
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error fetching employeetype :', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.json(results);
+    });
+})
+
+
+module.exports = router;
