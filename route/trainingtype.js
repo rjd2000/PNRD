e41c0a819c8abf93db02caa14606c6fea6d6451f
rjd2000 <borahrajdeep726@gmail.com>
2025-07-18 16:37:11 +0530

    Create trainingtype.js

---
+++ route/trainingtype.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('../database');
+const router = express.Router();
+ 
+router.get('/typ', (req, res) => {
+    const query = 'SELECT * FROM trainingtype ';
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error fetching trainingtype :', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.json(results);
+    });
+})
+
+
+module.exports = router;
