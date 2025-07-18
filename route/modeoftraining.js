e255302a7029fbe4b4736e49efd23197a2c6141f
rjd2000 <borahrajdeep726@gmail.com>
2025-07-18 16:36:40 +0530

    Create modeoftraining.js

---
+++ route/modeoftraining.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('../database');
+const router = express.Router();
+ 
+router.get('/mod', (req, res) => {
+    const query = 'SELECT * FROM modeoftraining ';
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error fetching modeoftraining :', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.json(results);
+    });
+})
+
+
+module.exports = router;
