7b0e39b5c8195fa40e6bc0e1619b501fbf93c378
rjd2000 <borahrajdeep726@gmail.com>
2025-07-18 23:54:02 +0530

    Update Participants.js

--- route/Participants.js
+++ route/Participants.js
@@ -3,10 +3,10 @@ const db = require('../database');
 const router = express.Router();
  
 router.get('/par', (req, res) => {
-    const query = 'SELECT * FROM participants';
+    const query = 'SELECT * FROM Participants';
     db.query(query, (err, results) => {
         if (err) {
-            console.error('Error fetching participants :', err);
+            console.error('Error fetching Participants :', err);
             return res.status(500).json({ error: 'Internal server error' });
         }
         res.json(results);
