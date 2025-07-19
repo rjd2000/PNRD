6e2731ba94ac7873e71a6e9caa34f79bc31ced5c
rjd2000 <borahrajdeep726@gmail.com>
2025-07-19 10:19:49 +0530

    Create training.js

---
+++ route/training.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('../database');
+const router = express.Router();
+ 
+router.get('/tee', (req, res) => {
+    const query = 'SELECT * FROM Training ';
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error fetching Training :', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.json(results);
+    });
+})
+
+
+module.exports = router;
