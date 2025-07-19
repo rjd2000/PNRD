fc60f0c1008dfd87acfe0ceb881bd7bfe00646db
rjd2000 <borahrajdeep726@gmail.com>
2025-07-19 10:39:19 +0530

    Create coordinator.js

---
+++ route/coordinator.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('../database');
+const router = express.Router();
+ 
+router.get('/cor', (req, res) => {
+    const query = 'SELECT * FROM coordinator ';
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error fetching coordinator :', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.json(results);
+    });
+})
+
+
+module.exports = router;
