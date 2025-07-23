20b457444b7c85b420a25413769675617cad99b3
rjd2000 <borahrajdeep726@gmail.com>
2025-07-23 13:41:53 +0530

    Update category.js

--- route/category.js
+++ route/category.js
@@ -13,5 +13,18 @@ router.get('/cat', (req, res) => {
     });
 })
 
+router.post('/co', (req, res) => {
+    const { CategoryID, CategoryName  } = req.body;
+    const query = 'INSERT INTO category (CategoryID, CategoryName) VALUES (?, ?)';
+    
+    db.query(query, [CategoryID, CategoryName ], (err, results) => {
+        if (err) {
+            console.error('Error inserting category:', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.status(201).json({ message: 'category added successfully', id: results.insertId });
+    });
+});
+
 
 module.exports = router;
