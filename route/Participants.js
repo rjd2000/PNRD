ac5ae2b9ab9b6f9bfe82e3131fc3506ca72f1075
rjd2000 <borahrajdeep726@gmail.com>
2025-07-26 13:36:02 +0530

    Update Participants.js

--- route/Participants.js
+++ route/Participants.js
@@ -12,6 +12,18 @@ router.get('/par', (req, res) => {
         res.json(results);
     });
 })
+router.post('/ch', (req, res) => {
+    const { ParticipationID, Name,CategoryID,SubCategory,Organization,DistrictID,Block,GaonPanchayat,GenderID,CasteID,Phone,EmailID,TrainingID } = req.body;
+    const query = 'INSERT INTO Participants (ParticipationID, Name,CategoryID,SubCategory,Organization,DistrictID,Block,GaonPanchayat,GenderID,CasteID,Phone,EmailID,TrainingID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    
+    db.query(query, [ParticipationID, Name,CategoryID,SubCategory,Organization,DistrictID,Block,GaonPanchayat,GenderID,CasteID,Phone,EmailID,TrainingID ], (err, results) => {
+        if (err) {
+            console.error('Error inserting Participants:', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.status(201).json({ message: 'Participants added successfully', id: results.insertId });
+    });
+});
 
 
 module.exports = router;
