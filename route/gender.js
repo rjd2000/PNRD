c961989f622c05c72875f2212251a1c7a17e5d8a
rjd2000 <borahrajdeep726@gmail.com>
2025-07-26 13:03:01 +0530

    Update gender.js

--- route/gender.js
+++ route/gender.js
@@ -12,6 +12,18 @@ router.get('/gen', (req, res) => {
         res.json(results);
     });
 })
+router.post('/ch', (req, res) => {
+    const { GenderID , GenderName  } = req.body;
+    const query = 'INSERT INTO gender (GenderID , GenderName) VALUES (?, ?)';
+    
+    db.query(query, [GenderID , GenderName ], (err, results) => {
+        if (err) {
+            console.error('Error inserting gender:', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.status(201).json({ message: 'gender added successfully', id: results.insertId });
+    });
+});
 
 
 module.exports = router;
