6585151ba4cde9d5d087ad2dd0a34aa8bdc5d67e
rjd2000 <borahrajdeep726@gmail.com>
2025-07-26 12:55:26 +0530

    Update employeetype.js

--- route/employeetype.js
+++ route/employeetype.js
@@ -12,6 +12,18 @@ router.get('/eee', (req, res) => {
         res.json(results);
     });
 })
+router.post('/ch', (req, res) => {
+    const { EmployeeTypeID , EmployeeType  } = req.body;
+    const query = 'INSERT INTO employeetype (EmployeeTypeID , EmployeeType) VALUES (?, ?)';
+    
+    db.query(query, [EmployeeTypeID , EmployeeType ], (err, results) => {
+        if (err) {
+            console.error('Error inserting employeetype:', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.status(201).json({ message: 'employeetype added successfully', id: results.insertId });
+    });
+});
 
 
 module.exports = router;
