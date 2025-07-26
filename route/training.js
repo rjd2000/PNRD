2fda3b6ac0983fa9ba6f02e6f0165ad2c2c12dde
rjd2000 <borahrajdeep726@gmail.com>
2025-07-26 20:13:07 +0530

    Update training.js

--- route/training.js
+++ route/training.js
@@ -12,6 +12,18 @@ router.get('/tee', (req, res) => {
         res.json(results);
     });
 })
+router.post('/ch', (req, res) => {
+    const { TrainingID,TrainingTitle,StartDate,EndDate,CourseID,TargetedNosOfParticipant,TypeOfTraining,ModeOfTraining,CoordinatorID,CentreID,FinancialYearID } = req.body;
+    const query = 'INSERT INTO Training (TrainingID,TrainingTitle,StartDate,EndDate,CourseID,TargetedNosOfParticipant,TypeOfTraining,ModeOfTraining,CoordinatorID,CentreID,FinancialYearID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    
+    db.query(query, [TrainingID,TrainingTitle,StartDate,EndDate,CourseID,TargetedNosOfParticipant,TypeOfTraining,ModeOfTraining,CoordinatorID,CentreID,FinancialYearID], (err, results) => {
+        if (err) {
+            console.error('Error inserting Training:', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.status(201).json({ message: 'Training added successfully', id: results.insertId });
+    });
+});
 
 router.delete('/cc/:TrainingID', (req, res) => {
     const { TrainingID } = req.params;
