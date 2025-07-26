98686ac5e8933cfc933f71d7d392f620b706ff19
rjd2000 <borahrajdeep726@gmail.com>
2025-07-26 12:40:17 +0530

    Update district.js

--- route/district.js
+++ route/district.js
@@ -12,6 +12,18 @@ router.get('/dis', (req, res) => {
         res.json(results);
     });
 })
+router.post('/ch', (req, res) => {
+    const { DistrictID , DistrictName  } = req.body;
+    const query = 'INSERT INTO district (DistrictID , DistrictName) VALUES (?, ?)';
+    
+    db.query(query, [DistrictID , DistrictName ], (err, results) => {
+        if (err) {
+            console.error('Error inserting district:', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.status(201).json({ message: 'district added successfully', id: results.insertId });
+    });
+});
 
 
 module.exports = router;
