1c69d09ab692d5bd7aff010f6ae4306b0983970f
rjd2000 <borahrajdeep726@gmail.com>
2025-07-26 18:33:01 +0530

    Update financialyear.js

--- route/financialyear.js
+++ route/financialyear.js
@@ -12,18 +12,7 @@ router.get('/fi', (req, res) => {
         res.json(results);
     });
 })
-router.post('/ch', (req, res) => {
-    const { FinancialYearID , FinancialYear  } = req.body;
-    const query = 'INSERT INTO financialyear (FinancialYearID , FinancialYear) VALUES (?, ?)';
-    
-    db.query(query, [FinancialYearID , FinancialYear ], (err, results) => {
-        if (err) {
-            console.error('Error inserting financialyear:', err);
-            return res.status(500).json({ error: 'Internal server error' });
-        }
-        res.status(201).json({ message: 'financialyear added successfully', id: results.insertId });
-    });
-});
+
 router.delete('/cc/:FinancialYearID', (req, res) => {
     const { FinancialYearID } = req.params;
     const query = 'DELETE FROM financialyear WHERE FinancialYearID = ?';
