f35fbf743114b706cb1c77faf61c56dac7a3adfc
rjd2000 <borahrajdeep726@gmail.com>
2025-07-26 18:30:54 +0530

    Update employeetype.js

--- route/employeetype.js
+++ route/employeetype.js
@@ -12,18 +12,7 @@ router.get('/eee', (req, res) => {
         res.json(results);
     });
 })
-router.post('/ch', (req, res) => {
-    const { EmployeeTypeID , EmployeeType  } = req.body;
-    const query = 'INSERT INTO employeetype (EmployeeTypeID , EmployeeType) VALUES (?, ?)';
-    
-    db.query(query, [EmployeeTypeID , EmployeeType ], (err, results) => {
-        if (err) {
-            console.error('Error inserting employeetype:', err);
-            return res.status(500).json({ error: 'Internal server error' });
-        }
-        res.status(201).json({ message: 'employeetype added successfully', id: results.insertId });
-    });
-});
+
 router.delete('/cc/:EmployeeTypeID', (req, res) => {
     const { EmployeeTypeID } = req.params;
     const query = 'DELETE FROM employeetype WHERE EmployeeTypeID = ?';
