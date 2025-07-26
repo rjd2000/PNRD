ee579d883f7499ae8f092034cbfec402ea79ffa8
rjd2000 <borahrajdeep726@gmail.com>
2025-07-26 17:09:30 +0530

    Update modeoftraining.js

--- route/modeoftraining.js
+++ route/modeoftraining.js
@@ -24,6 +24,22 @@ router.post('/ch', (req, res) => {
         res.status(201).json({ message: 'modeoftraining added successfully', id: results.insertId });
     });
 });
+router.delete('/cc/:TrainingModeID', (req, res) => {
+    const { TrainingModeID } = req.params;
+    const query = 'DELETE FROM modeoftraining WHERE TrainingModeID = ?';
+
+    db.query(query, [TrainingModeID], (err, results) => {
+        if (err) {
+            console.error('Error deleting modeoftraining:', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        if (results.affectedRows === 0) {
+            return res.status(404).json({ message: 'Mode of training not found' });
+        }
+        res.status(200).json({ message: 'Mode of training deleted successfully' });
+    });
+});
+
 
 
 module.exports = router;
