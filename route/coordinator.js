257bde7ebc537498d641c7db6a2a0dda0ede905e
rjd2000 <borahrajdeep726@gmail.com>
2025-07-26 12:08:29 +0530

    Update coordinator.js

--- route/coordinator.js
+++ route/coordinator.js
@@ -12,6 +12,19 @@ router.get('/cor', (req, res) => {
         res.json(results);
     });
 })
+router.post('/ch', (req, res) => {
+    const { CoordinatorID , EmployeeID   } = req.body;
+    const query = 'INSERT INTO coordinator (CoordinatorID , EmployeeID ) VALUES (?, ?)';
+    
+    db.query(query, [CoordinatorID , EmployeeID ], (err, results) => {
+        if (err) {
+            console.error('Error inserting coordinator:', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.status(201).json({ message: 'coordinator added successfully', id: results.insertId });
+    });
+});
+
 
 
 module.exports = router;
